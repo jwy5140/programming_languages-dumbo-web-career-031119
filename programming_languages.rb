73ec8c73dcab1ebe42312e9a7bd5b3138e2f5764
Jeffrey Yu <jwy5140@gmail.com>
2019-03-07 06:52:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,6 +4,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each_with_index {|(x,y),z| 
     y.each {|x,y|
+<<<<<<< HEAD
       if !new_hash[x]
         new_hash[x] = y
         new_hash[x][:style] = []
@@ -12,4 +13,11 @@ def reformat_languages(languages)
     }
   }
   new_hash
+=======
+      new_hash[x] = y
+      new_hash[x][:style] = (languages.keys)[z]
+    }
+  }
+  binding.pry
+>>>>>>> 99a4e9f4144bcba804fc0a74f4d6ae8e4c2f0bf6
 end
